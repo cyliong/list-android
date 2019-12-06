d78733acec733646f9ac40a641ccb294039a4491
cyliong <liongtheprogrammer@gmail.com>
2019-12-06 13:50:46 +0800

    Reactively enable or disable save button upon text changes

--- app/src/main/java/com/example/ltp/list/ItemActivity.kt
+++ app/src/main/java/com/example/ltp/list/ItemActivity.kt
@@ -5,14 +5,19 @@ import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.example.ltp.list.viewmodel.ItemViewModel
+import com.jakewharton.rxbinding3.widget.textChanges
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
 import kotlinx.android.synthetic.main.activity_item.*
 import kotlinx.android.synthetic.main.content_item.*
-import org.jetbrains.anko.alert
-import org.jetbrains.anko.yesButton
+import java.util.concurrent.TimeUnit
 
 class ItemActivity : AppCompatActivity() {
 
+    private val disposables = CompositeDisposable()
+
     private lateinit var viewModel: ItemViewModel
+    private var saveItem: MenuItem? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,8 +26,6 @@ class ItemActivity : AppCompatActivity() {
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-        edit_text_item_title.requestFocus()
-
         val itemId = intent.getStringExtra(EXTRA_ITEM_ID)
         viewModel = ItemViewModel(itemId)
         if (itemId == null) {
@@ -30,29 +33,37 @@ class ItemActivity : AppCompatActivity() {
         } else {
             edit_text_item_title.append(viewModel.title)
         }
+
+        edit_text_item_title.run {
+            requestFocus()
+
+            val disposable = textChanges()
+                .throttleLatest(100, TimeUnit.MILLISECONDS, true)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe { charSequence ->
+                    saveItem?.isEnabled = !charSequence.isNullOrBlank()
+                }
+            disposables.add(disposable)
+        }
     }
 
     override fun onDestroy() {
+        disposables.clear()
         viewModel.onDestroy()
         super.onDestroy()
     }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.activity_item, menu)
+        saveItem = menu.findItem(R.id.action_save)
         return super.onCreateOptionsMenu(menu)
     }
 
     override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
         R.id.action_save -> {
-            val title = edit_text_item_title.text?.toString()
-            if (title.isNullOrBlank()) {
-                alert("Please enter an item.") { yesButton {} }.show()
-                false
-            } else {
-                viewModel.onSave(title)
-                finish()
-                true
-            }
+            viewModel.onSave(edit_text_item_title.text.toString())
+            finish()
+            true
         }
         else -> {
             super.onOptionsItemSelected(item)
