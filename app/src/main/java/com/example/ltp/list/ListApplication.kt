ada958ca9ba9276093bd23affab9a2a1b1e4e9d0
cyliong <liongtheprogrammer@gmail.com>
2021-01-06 23:20:04 +0800

    Set default Realm configuration with migration settings

--- app/src/main/java/com/example/ltp/list/ListApplication.kt
+++ app/src/main/java/com/example/ltp/list/ListApplication.kt
@@ -1,13 +1,20 @@
 package com.example.ltp.list
 
 import androidx.multidex.MultiDexApplication
+import com.example.ltp.list.db.Migration
 import io.realm.Realm
+import io.realm.RealmConfiguration
 
 class ListApplication : MultiDexApplication() {
 
     override fun onCreate() {
         super.onCreate()
         Realm.init(this)
+        val config = RealmConfiguration.Builder()
+            .schemaVersion(1)
+            .migration(Migration())
+            .build()
+        Realm.setDefaultConfiguration(config)
     }
 
 }
